ba6318505c57f4b819768e758b9f5c264be3c9b5
delphifeel <delphifeel@gmail.com>
2022-10-02 19:01:11 +0300

    csv added

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/base64"
+	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -15,6 +19,36 @@ func makeToken(username string, password string) string {
 	sum := sha256.Sum256([]byte(s))
 	encoded := base64.StdEncoding.EncodeToString(sum[:])
 	return encoded
+}
+
+func testCreds(username string, password string) bool {
+	f, err := os.Open("users.csv")
+	if err != nil {
+		panic(err)
+	}
+
+	passwordB64 := base64.StdEncoding.EncodeToString([]byte(password))
+	csvReader := csv.NewReader(f)
+	csvReader.Comma = ';'
+
+	for {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		expectedUsername := record[0]
+		expectedPasswordB64 := record[1]
+
+		if expectedUsername == username && expectedPasswordB64 == passwordB64 {
+			return true
+		}
+	}
+
+	return false
 }
 
 func loginHandler(w http.ResponseWriter, req *http.Request) {
@@ -31,16 +65,20 @@ func loginHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	token := makeToken(username, password)
+	if ok := testCreds(username, password); !ok {
+		http.Error(w, "Wrong creds", 400)
+		return
+	}
 
+	token := makeToken(username, password)
 	fmt.Fprintf(w, token)
 }
 
 func main() {
 	http.HandleFunc("/login", loginHandler)
 
-	fmt.Println("pjlab_auth service started.")
+	log.Println("pjlab_auth service started.")
 	if err := http.ListenAndServe(":8090", nil); err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 }
