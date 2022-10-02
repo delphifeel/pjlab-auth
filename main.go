7801786e509a7a3d89d8583ae2fad756027a8cf8
delphifeel <delphifeel@gmail.com>
2022-10-02 18:22:36 +0300

    bearer auth added

--- main.go
+++ main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 	"net/http"
+	"time"
 )
 
+func makeToken(username string, password string) string {
+	now := time.Now()
+	year, month, day := now.Date()
+	s := fmt.Sprintf("%s:%s:%d:%d:%d", username, password, year, month, day)
+	sum := sha256.Sum256([]byte(s))
+	encoded := base64.StdEncoding.EncodeToString(sum[:])
+	return encoded
+}
+
 func loginHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Hello Mr. Someone\n")
+	w.Header().Set("Connection", "close")
+
+	username := req.FormValue("username")
+	if username == "" {
+		http.Error(w, "No username", 400)
+		return
+	}
+	password := req.FormValue("password")
+	if password == "" {
+		http.Error(w, "No password", 400)
+		return
+	}
+
+	token := makeToken(username, password)
+
+	fmt.Fprintf(w, token)
 }
 
 func main() {
 	http.HandleFunc("/login", loginHandler)
 
 	fmt.Println("pjlab_auth service started.")
-	err := http.ListenAndServe(":8090", nil)
-	if err != nil {
+	if err := http.ListenAndServe(":8090", nil); err != nil {
 		fmt.Println(err)
 	}
 }
